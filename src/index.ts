84a1ae5b60b6e1368cbc7150f7ff46be068583f0
egstar <76433966+egstar@users.noreply.github.com>
2022-06-24 00:10:40 +0200

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import fs from 'fs';
+import path from 'path';
+import * as dotenv from 'dotenv';
+import routes from './routes';
+
+const app = express(); // use the Express middleware
+
+dotenv.config(); // enables .env config file
+const port = process.env.port; // set port via .env file
+
+app.use(routes); // Use routes
+// Run server
+app.listen(port, () => {
+  // Check the thumbnails dir
+  const thumbdir = path.resolve(__dirname, '../assets/org/');
+  if (!fs.existsSync(thumbdir)) {
+    fs.mkdirSync(thumbdir);
+  }
+  console.log(`WORKING on port [${port}]`);
+});
+
+export default app;
