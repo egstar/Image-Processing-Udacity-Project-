01faf2cc87bc376442365176cec6246768fe0eae
egstar <76433966+egstar@users.noreply.github.com>
2022-06-24 00:14:06 +0200

    Create about.ts

---
+++ src/routes/user/about.ts
@@ -0,0 +1,10 @@
+import { Router, Request, Response } from 'express';
+import path from 'path';
+
+const aboutRoute = Router();
+// set Manual route
+aboutRoute.get('/resize', (_req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, '../../../assets/htm/resize.htm'));
+});
+
+export default aboutRoute;
