e8ca885590feeff50335eb82362bf5cc2214275d
egstar <76433966+egstar@users.noreply.github.com>
2022-06-24 23:16:48 +0200

    Add files via upload

--- src/utils/imgproc.ts
+++ src/utils/imgproc.ts
@@ -6,9 +6,16 @@ interface imgproc {
   w: number;
 }
 const resizeImg = async ({ fimgp, tnp, h, w }: imgproc) => {
-  if (fimgp && w && h && tnp) await sharp(fimgp).resize(w, h).toFile(tnp);
-
-  return tnp;
+  if (fimgp && w && h && tnp) {
+    await sharp(fimgp).resize(w, h).toFile(tnp);
+    return tnp;
+  }
+  if (!fimgp) {
+    return `Error, [ ${fimgp} ] is not a valid image name`;
+  }
+  if (!w && !h) {
+    return `Error, Please Specify a valid numbers in the [Width & Height] fields`;
+  }
 };
 
 export default resizeImg;
