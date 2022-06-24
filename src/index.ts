60959dcfdc52c84f681f67c397cc39f5eabaaa45
egstar <76433966+egstar@users.noreply.github.com>
2022-06-24 09:05:53 +0200

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,11 @@ import routes from './routes';
 const app = express(); // use the Express middleware
 
 dotenv.config(); // enables .env config file
-const port = process.env.port; // set port via .env file
+const port = process.env.port || 3000; // set port via .env file or use the 3000 port instead if not available
 
 app.use(routes); // Use routes
-// Run server 
+// Run server
 app.listen(port, () => {
-  
   console.log(`WORKING on port [${port}]`);
 });
 
