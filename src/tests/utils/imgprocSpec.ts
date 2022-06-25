3330b023087ae56272f32f054c1e8b34fb31f70f
egstar <76433966+egstar@users.noreply.github.com>
2022-06-25 07:26:10 +0200

    Add files via upload

--- src/tests/utils/imgprocSpec.ts
+++ src/tests/utils/imgprocSpec.ts
@@ -1,7 +1,6 @@
 import resizeImg from '../../utils/imgproc';
 import path from 'path';
 
-
 const fimgp = path.resolve(__dirname, '../../../assets/org/fjord.jpg');
 const tnp = path.resolve(__dirname, '../../../assets/tn/200x200-fjord.jpg');
 
