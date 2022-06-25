44e9df4b4f04dc7de208b0fc11a2888d099eb5a6
egstar <76433966+egstar@users.noreply.github.com>
2022-06-25 04:25:22 +0200

    Add files via upload

--- src/utils/imgproc.ts
+++ src/utils/imgproc.ts
@@ -5,7 +5,7 @@ interface imgproc {
   h: number;
   w: number;
 }
-const resizeImg = async ({ fimgp, tnp, h, w }: imgproc) => {
+const resizeImg = async ({ fimgp, tnp, h, w }: imgproc): Promise<unknown> => {
   if (fimgp && w && h && tnp) {
     await sharp(fimgp).resize(w, h).toFile(tnp);
     return tnp;
