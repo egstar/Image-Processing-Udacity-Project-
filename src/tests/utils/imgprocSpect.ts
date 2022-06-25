1ab1b06bf9c788ee619637282f65217f4f050d8e
egstar <76433966+egstar@users.noreply.github.com>
2022-06-25 04:20:54 +0200

    Create imgprocSpect.ts

---
+++ src/tests/utils/imgprocSpect.ts
@@ -0,0 +1,17 @@
+import resizeImg from '../../utils/imgproc';
+import path from 'path';
+
+const fimgp = path.resolve(__dirname, '../../../assets/org/fjord.jpg');
+const tnp = path.resolve(__dirname, '../../../assets/tn/200x200-fjord.jpg');
+
+describe('Testing the Image processing', (): void => {
+  it('Processing Image with 200x200 size successfuly', async () => {
+    const myImage = (await resizeImg({
+      fimgp: fimgp,
+      tnp: tnp,
+      h: 200,
+      w: 200,
+    })) as string;
+    expect(myImage).toBe(tnp);
+  });
+});
